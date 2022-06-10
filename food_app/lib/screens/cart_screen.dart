1e1cd48ea14ebf2dc8022e2f0b9f381a6142f80f
Paul Q. Peters <paul.peters144@gmail.com>
2022-06-10 04:57:47 -0500

    added budget app

--- food_app/lib/screens/cart_screen.dart
+++ food_app/lib/screens/cart_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:food_app/data/data.dart';
+import 'package:food_app/modals/order.dart';
 
 class CartScreen extends StatefulWidget {
   const CartScreen({Key key}) : super(key: key);
@@ -15,6 +16,204 @@ class _CartScreenState extends State<CartScreen> {
       appBar: AppBar(
         title: Text('Cart (${currentUser.cart.length})'),
         centerTitle: true,
+      ),
+      body: ListView.separated(
+        itemCount: currentUser.cart.length + 1,
+        itemBuilder: ((context, index) => _buildCartItem(index, context)),
+        separatorBuilder: ((context, index) =>
+            const Divider(height: 1.0, color: Colors.grey)),
+      ),
+      bottomSheet: Container(
+        height: 100.0,
+        width: MediaQuery.of(context).size.width,
+        decoration: BoxDecoration(
+          color: Theme.of(context).primaryColor,
+          boxShadow: [
+            getBoxShadow(),
+          ],
+        ),
+        child: Center(
+          child: TextButton(
+            onPressed: () {},
+            child: const Text(
+              'Check Out',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 22.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  BoxShadow getBoxShadow() {
+    return const BoxShadow(
+      color: Colors.black26,
+      offset: Offset(0, -1),
+      blurRadius: 6.0,
+    );
+  }
+
+  _buildCartItem(int index, BuildContext context) {
+    if (index < currentUser.cart.length) {
+      return _orderContainer(currentUser.cart[index], context);
+    } else {
+      return _orderSummary(context);
+    }
+  }
+
+  _orderContainer(Order order, BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(20.0),
+      height: 170.0,
+      child: Row(
+        children: [
+          Expanded(
+            child: Row(
+              children: [
+                Container(
+                  width: 150.0,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage(order.food.imageUrl),
+                      fit: BoxFit.cover,
+                    ),
+                    borderRadius: BorderRadius.circular(15.0),
+                  ),
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          order.food.name,
+                          style: const TextStyle(
+                              fontSize: 20.0, fontWeight: FontWeight.bold),
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                        const SizedBox(height: 10.0),
+                        Text(
+                          order.restaurant.name,
+                          style: const TextStyle(
+                              fontSize: 16.0, fontWeight: FontWeight.w600),
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                        const SizedBox(height: 10.0),
+                        Container(
+                          width: 95,
+                          height: 25,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(8.0),
+                            border: Border.all(
+                              width: 1,
+                              color: Colors.black54,
+                            ),
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              GestureDetector(
+                                onTap: () {},
+                                child: Text(
+                                  "-",
+                                  style: TextStyle(
+                                      fontSize: 16.0,
+                                      fontWeight: FontWeight.w600,
+                                      color: Theme.of(context).primaryColor),
+                                ),
+                              ),
+                              const SizedBox(width: 20.0),
+                              Text(
+                                order.quantity.toString(),
+                                style: const TextStyle(
+                                    fontSize: 16.0,
+                                    fontWeight: FontWeight.w600),
+                              ),
+                              const SizedBox(width: 20.0),
+                              GestureDetector(
+                                onTap: () {},
+                                child: Text(
+                                  '+',
+                                  style: TextStyle(
+                                      fontSize: 16.0,
+                                      fontWeight: FontWeight.w600,
+                                      color: Theme.of(context).primaryColor),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.all(10.0),
+                  child: Text(
+                    "\$${order.quantity * order.food.price}",
+                    style: const TextStyle(
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.black87,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  _orderSummary(BuildContext context) {
+    double totalPrice = 0;
+    for (var order in currentUser.cart) {
+      totalPrice += (order.quantity * order.food.price);
+    }
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: const [
+              Text(
+                'Estimated Delivery Time:',
+                style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.w600),
+              ),
+              Text(
+                '25 min',
+                style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.w600),
+              ),
+            ],
+          ),
+          const SizedBox(height: 10.0),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const Text(
+                'Total Cost:',
+                style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.w600),
+              ),
+              Text(
+                '\$${totalPrice.toStringAsFixed(2)}',
+                style: const TextStyle(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.w600,
+                  color: Colors.green,
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 80.0),
+        ],
       ),
     );
   }
